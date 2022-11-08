4bef5abb75f4c3423b73021a1a01165a835de49c
Florim Hoxha <florimhoxha.mgs@gmail.com>
2022-11-08 16:50:28 +0000

    Update SQLite_database.py

--- SQLite/SQLite_database.py
+++ SQLite/SQLite_database.py
@@ -4,22 +4,22 @@ import sqlite3
 
 connection = sqlite3.connect('person.db')
 
-# cursor = connection.cursor()
+cursor = connection.cursor()
 
-# cursor.execute("INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1976)")
-# cursor.execute("SELECT * FROM Movies")
-# famousFilms = [('Puplp Fiction', 'Quinton Tarantino', 1994), ('Back to the Future', 'Steven Spielberg', 1985),\
-#                 ('Moonrise Kingdom', 'Wes Anderson', 2012)]
+cursor.execute("INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1976)")
+cursor.execute("SELECT * FROM Movies")
+famousFilms = [('Puplp Fiction', 'Quinton Tarantino', 1994), ('Back to the Future', 'Steven Spielberg', 1985),\
+                ('Moonrise Kingdom', 'Wes Anderson', 2012)]
 
-# cursor.executemany("INSERT INTO Movies VALUES (?,?,?)", famousFilms)
-# records = cursor.execute("SELECT * FROM Movies")
+cursor.executemany("INSERT INTO Movies VALUES (?,?,?)", famousFilms)
+records = cursor.execute("SELECT * FROM Movies")
 
-# print(cursor.fetchall())
+print(cursor.fetchall())
 
-# for record in records:
-#     print(record)
-# release_year = (1985,)
-# cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
-# print(cursor.fetchall())
+for record in records:
+    print(record)
+release_year = (1985,)
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
+print(cursor.fetchall())
 connection.commit()
 connection.close()
